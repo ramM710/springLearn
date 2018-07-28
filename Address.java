4c11d897012d31864394cab02e8d1c4f9e6d41c9
ramM710 <40501239+ramM710@users.noreply.github.com>
2018-07-29 01:09:14 +0530

    Create Address.java
    added address object class. It was added as a Dependency injection example

---
+++ Address.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package emp;
+
+/**
+ *
+ * @author Ram M
+ */
+public class EmpAddress {
+
+    String city;
+    String country;
+    int zipCode;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public String toString() {
+        return "EmpAddress{" + "city=" + city + ", country=" + country + ", zipCode=" + zipCode + '}';
+    }
+
+}
